36073da94baff6b21b211aa390dc8e28982390ac
AKIKO <147792679+akkTYO@users.noreply.github.com>
2025-07-01 10:05:06 +0900

    Update script.js

--- section-05-digital-image-processing/filtering-image-collection/script.js
+++ section-05-digital-image-processing/filtering-image-collection/script.js
@@ -6,7 +6,7 @@ var roi = ee.Geometry.Point([30.68, -1.195]);
 // 2. Define scale factor function for Landsat imagery
 function applyscaleFactors(image){
   var opticalBands = image.select("SR_B.").multiply(0.0000275).add(-0.2);
-  var thermalBands = image.select("SR_B.*").multiply(0.00341802).add(-149.0);
+  var thermalBands = image.select("ST_B.*").multiply(0.00341802).add(-149.0);
   return image.addBands(opticalBands, null, true)
               .addBands(thermalBands, null, true);
 }
@@ -24,3 +24,17 @@ var filtered = l9
   .filter(ee.Filter.bounds(roi));
 
 print("After filtering:", filtered.size());
+
+// 5. Select the first image from the filtered collection
+var firstImage = filtered.sort("CLOUD_COVER").first();
+
+// 6. Visualization parameters for scaled reflectance (True Color)
+var visualization = {
+bands: ["SR_B4", "SR_B3", "SR_B2"],
+min: 0.0,
+max: 0.3
+};
+
+// 7. Display the selected image on the map
+Map.centerObject(firstImage, 8);
+Map.addLayer(firstImage, visualization, "Filtered Image RGB");
